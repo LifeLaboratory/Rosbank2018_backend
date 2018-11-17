6525360e945f650931b8b5558ddd1f7a4673561b
raldenprog <raldenprog@mail.ru>
2018-11-18 06:09:55 +0700

    fix config

--- quotation/config/config.py
+++ quotation/config/config.py
@@ -2,10 +2,10 @@ HEADER = {'Access-Control-Allow-Origin': '*'}
 DATABASE = {
     "dbname": "ARM",
     "user": "ARM",
-    "host": "90.189.132.25",
-    # "host": "192.168.1.3",
+    # "host": "90.189.132.25",
+    "host": "192.168.1.3",
     # "host": "127.0.0.1",
     "password": "ARM_SYSTEM"
 }
 
-HOST = "http://0.0.0.0:13452"
+HOST = "http://90.189.132.25"
