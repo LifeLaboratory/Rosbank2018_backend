ceed94b7a0f5dd4c98544ce972b8f2dcbf54dcfb
Anton Chusovitin <antonion2011@yandex.ru>
2018-11-17 18:44:24 +0700

    * Фикс сервисов

--- auth/route/RouteFavicon.py
+++ auth/route/RouteFavicon.py
@@ -1,10 +1,10 @@
 # coding=utf-8
 from flask_restful import Resource
-from quotation.api.src.quotation_method import *
 
 
 class Favicon(Resource):
     def get(self):
-        return "OK", errors.OK, {'Access-Control-Allow-Origin': '*'}
+        return "OK", 200, {'Access-Control-Allow-Origin': '*'}
+
     def option(self):
-        return "OK", errors.OK, {'Access-Control-Allow-Origin': '*'}
+        return "OK", 200, {'Access-Control-Allow-Origin': '*'}
--- auth/route/route_list.py
+++ auth/route/route_list.py
@@ -1,5 +1,5 @@
 from auth.route.RouteAuth import Service
-from quotation.route.RouteFavicon import Favicon
+from auth.route.RouteFavicon import Favicon
 
 ROUTES = {
     Service: '/api/v1/auth',
